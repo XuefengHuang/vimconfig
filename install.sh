067827de187e66752abd4033255fd129973b11c6
huangxuefeng01 <huangxuefeng01@baidu.com>
2019-08-23 11:55:09 +0800

    add php intelephense

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#install php intelephense
+npm i intelephense -g
