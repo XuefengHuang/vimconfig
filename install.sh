c796d196f56253f66726bff4aae3002ce6ce5000
huangxuefeng01 <huangxuefeng01@baidu.com>
2019-08-26 14:22:42 +0800

    add ag

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #install php intelephense
 npm i intelephense -g
+#install ag
+brew install the_silver_searcher
